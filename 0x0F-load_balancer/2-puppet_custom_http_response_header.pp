bcd2f4f527eaed76dab401e38c6721bd04ee2444
Levyslater <lawre@Xshader>
2024-07-28 13:58:52 +0300

    load_balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+# install nginx and configure a custom header response
+
+exec { 'update':
+	command => 'sudo apt-get update''
+	provider => shell,
+}
+
+package { 'nginx':
+	ensure => installed,
+	require => Exec['update'],
+}
+
+file_line { 'headercustom':
+	ensure => present,
+	path => 'etc/nginx/sites-available/default',
+	after => ':80 default_server;',
+	line => "add_header X-Served-By ${hostname};",
+	require => Package['nginx'],
+}
+
+service { 'nginx':
+	ensure => running,
+	require => file_line['headercustom'],
+}
