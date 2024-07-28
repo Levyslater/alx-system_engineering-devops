728dd3ef1df71a1a106a157604be43ffbb5ae121
Levyslater <lawre@Xshader>
2024-07-28 15:55:43 +0300

    load_balancing_puppet2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,10 @@
 #!usr/bin/env bash
 # install nginx and configure a custom header response
 
-exec { 'update':
-	command => 'sudo apt-get update''
-	provider => shell,
+exec { 'http header':
+	command => 'sudo apt-get update -y';
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart
 }
 
-package { 'nginx':
-	ensure => installed,
-	require => Exec['update'],
-}
-
-file_line { 'headercustom':
-	ensure => present,
-	path => 'etc/nginx/sites-available/default',
-	after => ':80 default_server;',
-	line => "add_header X-Served-By ${hostname};",
-	require => Package['nginx'],
-}
-
-service { 'nginx':
-	ensure => running,
-	require => file_line['headercustom'],
-}
