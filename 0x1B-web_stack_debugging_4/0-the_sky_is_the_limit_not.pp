ff413b0ac30de2ae90ac95fddf8c837b856b9b57
Levyslater <lawre@Xshader>
2024-08-25 04:23:24 +0300

    debugging final version add comments2

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,6 +2,5 @@
 exec { 'modify nginx max open files limit':
   command => 'sed -i "s/^#worker_rlimit_nofile.*/worker_rlimit_nofile 10000;/" /etc/nginx/nginx.conf && systemctl restart nginx',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => 'grep -q "^#worker_rlimit_nofile" /etc/nginx/nginx.conf',
 }
 
