1db80d6d26cbaedabeb8594b4ec207e3586cc03c
Levyslater <lawre@Xshader>
2024-08-25 04:18:00 +0300

    debugging final version add comments

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+# Modify nginx config file to increase tasks limit
 exec { 'modify nginx max open files limit':
   command => 'sed -i "s/^#worker_rlimit_nofile.*/worker_rlimit_nofile 10000;/" /etc/nginx/nginx.conf && systemctl restart nginx',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,5 @@
+# Change the OS configuration so that it is possible to login
+# with the holberton user and open a file without any error message
 exec { 'set file limits for holberton user':
   command => 'echo -e "holberton soft nofile 4096\nholberton hard nofile 8192" >> /etc/security/limits.conf',
   path    => '/usr/bin/env:/bin:/usr/bin:/usr/sbin',
