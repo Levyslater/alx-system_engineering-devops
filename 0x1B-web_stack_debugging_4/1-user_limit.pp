355733d6e05650db8fdbe8877dae27d6b31ed746
Levyslater <lawre@Xshader>
2024-08-25 04:21:29 +0300

    debugging final version add comments1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,7 @@
-# Change the OS configuration so that it is possible to login
-# with the holberton user and open a file without any error message
-exec { 'set file limits for holberton user':
-  command => 'echo -e "holberton soft nofile 4096\nholberton hard nofile 8192" >> /etc/security/limits.conf',
-  path    => '/usr/bin/env:/bin:/usr/bin:/usr/sbin',
-  unless  => 'grep -q "^holberton.*nofile" /etc/security/limits.conf',
-}
+# Change the OS configuration so that it is possible to login with the
+# holberton user and open a file without any error message.
 
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
